20845a778be7e2b9db9b69afdc4a98b48faf9738
dubiaopei <dubiaopei@gmail.com>
2015-06-23 19:26:38 +0800

    分布式session实现
    使用前请在web.xml中配置：sessionFilter
    
    使用方法请详细见:
    src/test/resources中的:spring-session.xml此为spring配置文件
    
    session.properties:此为session store的配置

---
+++ src/main/java/com/dumpcache/sessionanywhere/SessionIDMaker.java
@@ -0,0 +1,12 @@
+package com.dumpcache.sessionanywhere;
+
+/**
+ * sessionId maker
+ * 
+ * @author dubiaopei
+ * @date 2014-6-18 上午11:23:58
+ */
+public interface SessionIDMaker {
+
+    public String makeNewId();
+}
